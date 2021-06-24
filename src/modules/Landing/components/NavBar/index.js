0b05287a97b79e4a02a1a93e1185023917c40254
Carol <carol_cardenas93@hotmail.com>
2021-06-24 15:25:06 -0500

    create button component and change navbar layout

--- src/modules/Landing/components/NavBar/index.js
+++ src/modules/Landing/components/NavBar/index.js
@@ -3,28 +3,38 @@ import Logo from "../../../../assets/img/png/logo.png";
 import labelsNavBar from "./constants";
 import User from "../../../../assets/img/png/user.png";
 import "./style.css";
+import Button from "../Button";
 
 
 const NavBar = () => {
   return (
     <div className="nav-bar-container">
       <div className="nav-bar-logo">
-        <img src={Logo} alt="Logo"/>
+        <img src={Logo} alt="Logo" />
       </div>
       <div className="nav-bar-label">
         <ul className="nav-bar-label-ul">
-          {labelsNavBar.map((label,index)=>{
-            return(
-              <li key={`navbar-label-${index}`}>{label.name}</li>
-            )
+          {labelsNavBar.map((label, index) => {
+            return (
+              <li key={`navbar-label-${index}`}>
+                <Button type="link" text={label.name} link={label.url} />
+              </li>
+            );
           })}
         </ul>
+
       </div>
       <div className="nav-bar-icons">
         <div>
-          <img src={User} alt="Linea Ancestral Logo" className="nav-bar-user-icon"/>
+          <img
+            src={User}
+            alt="Linea Ancestral Logo"
+            className="nav-bar-user-icon"
+          />
         </div>
       </div>
+      <Button type="primary" text="Crear Proyecto"></Button>
+      <Button type="secundary" text="Login"></Button>
     </div>
   );
 }
