966cd329a4866390b75da9f1cb6df8a93e99cc28
Carol <carol_cardenas93@hotmail.com>
2021-07-12 15:58:26 -0500

    section 3

--- src/modules/Landing/components/Benefit/constants.js
+++ src/modules/Landing/components/Benefit/constants.js
@@ -1,5 +1,5 @@
 const benefitConstants = {
-  title: "¿Por qué tener MY ADFI?",
+  title: "¿Por qué elegir MY ADFI?",
   paragraph1:"1 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
   paragraph2:"2 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
   paragraph3:"3 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
