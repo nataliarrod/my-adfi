88c7e5b1a57f9171a8c9c520e798b2056230faaa
Carol <carol_cardenas93@hotmail.com>
2021-07-13 14:39:14 -0500

    New container-content

--- src/modules/Landing/components/Benefit/index.js
+++ src/modules/Landing/components/Benefit/index.js
@@ -10,23 +10,25 @@ const Benefit = () => {
 return (
   <div className="benefit-container">
     <div className="benefit-title">{benefitConstants.title}</div>
-    <div className="benefit-paragraphs">
-      <div className="benefit-paragraph1">{benefitConstants.paragraph1}</div>
-      <div className="benefit-paragraph2">
-        <div className="benefit-icon1">
-          <img src={icon1} alt="1-27" />
+    <div className="benefit-container-content">
+      <div className="benefit-paragraphs">
+        <div className="benefit-paragraph1">{benefitConstants.paragraph1}</div>
+        <div className="benefit-paragraph2">
+          <div className="benefit-icon1">
+            <img src={icon1} alt="1-27" />
+          </div>
+          <div className="benefit-text2">{benefitConstants.paragraph2}</div>
         </div>
-        <div className="benefit-text2">{benefitConstants.paragraph2}</div>
-      </div>
-      <div className="benefit-paragraph3">
-        <div className="benefit-icon2">
-          <img src={icon2} alt="1-11" />
+        <div className="benefit-paragraph3">
+          <div className="benefit-icon2">
+            <img src={icon2} alt="1-11" />
+          </div>
+          <div className="benefit-text3">{benefitConstants.paragraph3}</div>
         </div>
-        <div className="benefit-text3">{benefitConstants.paragraph3}</div>
       </div>
-    </div>
-    <div className="benefit-statisticsMascot">
-      <img src={statisticmascot} alt="mascot-04" />
+      <div className="benefit-statisticsMascot">
+        <img src={statisticmascot} alt="mascot-04" />
+      </div>
     </div>
   </div>
 );
