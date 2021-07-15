59fe4096a1f88eb021f2cdbd8b4c5faa99ae232d
Carol <carol_cardenas93@hotmail.com>
2021-07-15 18:07:00 -0500

    containers - countersection

--- src/modules/Landing/components/Counter/index.js
+++ src/modules/Landing/components/Counter/index.js
@@ -3,8 +3,19 @@ import "./style.css";
 
 const Counter = () => {
   return (
-    <div>
-      <h1>Counter</h1>
+    <div className="countersection-container">
+      <div className="countersection-image">
+
+      </div>
+      <div className="countersection-content">
+        <div className="countersection-content-count">
+
+        </div>
+        <div className="countersection-content-text">
+
+        </div>
+        <div className="countersection-content-swtich"></div>
+      </div>
     </div>
   )
 }
