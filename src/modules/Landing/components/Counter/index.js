6c6c9582221339c394b36e672ccb1bb6c725d00d
Carol <carol_cardenas93@hotmail.com>
2021-07-15 20:37:21 -0500

    image size, text content

--- src/modules/Landing/components/Counter/index.js
+++ src/modules/Landing/components/Counter/index.js
@@ -1,23 +1,32 @@
 import React from "react";
 import "./style.css";
+import mascot04 from "../../../../assets/img/svg/mascot-04.svg";
+import ReactDOM from "react-dom";
 
 const Counter = () => {
   return (
     <div className="countersection-container">
       <div className="countersection-image">
-
+        <img src={mascot04} alt="mascot-04" />
       </div>
       <div className="countersection-content">
         <div className="countersection-content-count">
-
+          <span class="text-primary">
+            +
+          </span>
+          <span class="counter">
+            80000
+          </span>
         </div>
         <div className="countersection-content-text">
-
+          Emprendedores están gestionando sus empresas
         </div>
-        <div className="countersection-content-swtich"></div>
+        <div className="countersection-content-swtich">
+          lleva el control de tu negocio
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default Counter;
