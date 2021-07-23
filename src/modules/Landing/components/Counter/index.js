19919313edd31007e7b57b9843661dae78d3cbfc
Carol <carol_cardenas93@hotmail.com>
2021-07-23 16:35:12 -0500

    jusfify countersection-content-switch

--- src/modules/Landing/components/Counter/index.js
+++ src/modules/Landing/components/Counter/index.js
@@ -1,28 +1,29 @@
 import React from "react";
 import "./style.css";
-import mascot04 from "../../../../assets/img/svg/mascot-04.svg";
+import imagecountersection from "../../../../assets/img/png/user.png";
 
 const Counter = () => {
   return (
     <div className="countersection-container">
       <div className="countersection-image">
-        <img src={mascot04} alt="mascot-04" />
+        <img src={imagecountersection} alt="user" />
       </div>
       <div className="countersection-content">
         <div className="countersection-content-count">
-          <span class="text-primary">
-            +
-          </span>
-          <span class="counter">
-            1000
-          </span>
+          <span class="text-primary">+</span>
+          <span class="counter">1000</span>
         </div>
         <div className="countersection-content-text">
           Emprendedores están <br />
           gestionando sus empresas
         </div>
         <div className="countersection-content-swtich">
-          lleva el control de tu negocio
+          <div className="countersection-content-switch-text">
+            Lleva el control de tu negocio
+          </div>
+          <div className="countersection-content-switch-swtchbutton">
+            
+          </div>
         </div>
       </div>
     </div>
