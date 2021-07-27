2c2903eba6aac6ba904d600b927d87c44b979bc7
Carol <carol_cardenas93@hotmail.com>
2021-07-26 22:42:09 -0500

    change mascot searchbar

--- src/modules/Landing/components/SearchBar/index.js
+++ src/modules/Landing/components/SearchBar/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import mascot04 from "../../../../assets/img/svg/mascot-04.svg";
+import mascot from "../../../../assets/img/png/mascotaprueba.png";
 import icon1_30 from "../../../../assets/img/svg/1-30.svg";
 import "./style.css";
 import searchBarConstants from "./constants";
@@ -9,13 +9,17 @@ const SearchBar = () => {
   return (
     <div className="searchbar-container">
       <div className="searchbar-mascot">
-        <img src={mascot04} alt="mascot-04" />
+        <img src={mascot} alt="mascotaprueba" />
       </div>
       <div className="searchbar-group">
         <div className="searchbar-title">{searchBarConstants.title}</div>
         <div className="searchbar-text">{searchBarConstants.text}</div>
         <div className="searchbar-search">
-          <input className="search-input" type="text" placeholder="Buscar" />
+          <input
+            className="search-input"
+            type="text"
+            placeholder="LOREM IPSUM IS SIMPLY"
+          />
           <button className="search-button" type="submit">
             <img src={icon1_30} alt="1-30" />
           </button>
