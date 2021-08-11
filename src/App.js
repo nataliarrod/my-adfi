93c1ee67eab58f894e93e9bed6cacee08486b316
nataliarrod <77126122+nataliarrod@users.noreply.github.com>
2021-08-11 15:28:27 -0500

    layout for landing was created (#11)
    * layout for landing was created

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import React from "react";
 import Landing from "./modules/Landing/pages/Home";
@@ -7,7 +7,7 @@ function App() {
   return (
     <Router>
       <Switch>
-        <Route exact path="/" component={Landing}/>
+        <Route exact path="/" component={Landing} />
       </Switch>
     </Router>
   );
